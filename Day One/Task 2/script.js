d5f8034fcd3ad9e998d786c24e287a288250195f
GRGHSS <142840593+GRGHSS@users.noreply.github.com>
2024-02-26 00:07:40 +0400

    Add files via upload

---
+++ Day One/Task 2/script.js
@@ -0,0 +1,8 @@
+// hghum knopkayi u abzaci vra
+const button = document.getElementById("changeTextButton");
+const paragraph = document.getElementById("paragraph");
+
+button.addEventListener("click", function() {
+    // Изменяем текстовое содержимое абзаца
+    paragraph.textContent = "text k poxvi hpman depqum";
+});
