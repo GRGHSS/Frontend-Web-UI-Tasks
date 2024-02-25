ace48341410dd42fbfb3d6cb5244c7536644322e
GRGHSS <142840593+GRGHSS@users.noreply.github.com>
2024-02-26 00:08:41 +0400

    Update script.js

--- Day One/Task 2/script.js
+++ Day One/Task 2/script.js
@@ -3,6 +3,6 @@ const button = document.getElementById("changeTextButton");
 const paragraph = document.getElementById("paragraph");
 
 button.addEventListener("click", function() {
-    // Изменяем текстовое содержимое абзаца
+   
     paragraph.textContent = "text k poxvi hpman depqum";
 });
